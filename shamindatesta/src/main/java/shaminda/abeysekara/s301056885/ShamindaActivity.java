a7c3a223b991c995fe26c6c7080f88405f5a9f15
Shaminda1017 <65966602+Shaminda1017@users.noreply.github.com>
2020-10-31 08:50:55 -0400

    shamindaTestA

---
+++ shamindatesta/src/main/java/shaminda/abeysekara/s301056885/ShamindaActivity.java
@@ -0,0 +1,15 @@
+package shaminda.abeysekara.s301056885;
+//Shaminda_Abeysekara_301056885_002
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShamindaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
